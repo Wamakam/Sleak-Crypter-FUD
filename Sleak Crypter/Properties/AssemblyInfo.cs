2d3b03bba9555b9596a318f475426998078ae0a6
Denis T. <deadspell21391@gmail.com>
2025-10-20 11:56:44 +0200

    Upload Files

---
+++ Sleak Crypter/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sleak Crypter Tool")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sleak Crypter Tool")]
+[assembly: AssemblyCopyright("© Sleak Corporation. All rights reserved")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("EE51CE51-50A6-40A7-9051-EB65F48D9BCC")]
+[assembly: AssemblyVersion("5.18.64.4119")]
+[assembly: AssemblyFileVersion("5.18.64.4119")]
